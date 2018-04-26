029b86f8049403974c36d5d2bcfc9c2c8bf4e1fe
Jaide King-Griffin <jaidekg@gmail.com>
2018-04-26 19:31:25 -0400

    first commit

---
+++ Lab Number6/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Number6
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Welcome to the Academic Games of Grand Circus!");
+                //ask user for dice sides
+                Console.WriteLine("How many sides are there?");
+                //asking the string to hold sides
+                string sides = Console.ReadLine();
+                //converting sides
+                int numberSides = int.Parse(sides);
+                Console.WriteLine("Do you want to roll the dice or exit?");
+                string response = Console.ReadLine();
+
+                //prompt user for dice roll
+                while (true)
+                {
+                    if (response == "exit")
+                    {
+                        Console.WriteLine("Thank you for participating! Goodbye");
+                        return;
+                    }
+                    else if (response == "y")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, that method isn't valid. Please enter 'y' or exit.");
+                        response = Console.ReadLine();
+
+                    }
+                }
+                int rolledNum = Rolldice(numberSides);
+                int rolledNum2 = Rolldice(numberSides);
+
+
+
+                Console.WriteLine($"1st number {rolledNum} 2nd number {rolledNum2}");
+                //display what they have rolled
+
+                //ask if user wants to continue
+                Console.WriteLine("Play again?");
+                response = Console.ReadLine();
+               
+
+                while (true)
+                {
+                    if (response == "exit")
+                    {
+                        Console.WriteLine("Thank you for participating! Goodbye");
+                        return;
+                    }
+                    else if (response == "y")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, that method isn't valid. Please enter 'y' or exit.");
+                        response = Console.ReadLine();
+
+                    }
+                }
+
+            }
+        }
+
+        static int Rolldice(int numberSides)
+        {
+            int num = rnd.Next(1,numberSides);
+
+            return num;
+            
+
+        }
+
+
+    }
+
+    
+
+}
