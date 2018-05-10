c79b87958ad88157fccc440b9b24d0f3e1f27ea4
Jaide King-Griffin <jaidekg@gmail.com>
2018-05-10 17:02:32 -0400

    first commit

---
+++ Lab Number6/DiceRollerApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Number6
+{
+    class DiceRollerApp
+    {
+        public static void CheckRoll(int x, int y)
+        {
+            if (x + y == 7)
+            {
+                Console.WriteLine("craps");
+                Console.ReadLine();
+            }
+            else if (x + y == 2)
+            {
+                Console.WriteLine("snake eyes");
+                Console.ReadLine();
+            }
+            else if (x + y == 12)
+            {
+                Console.WriteLine("box cars");
+                Console.ReadLine();
+
+
+            
+
+
+            }
+
+        }
+    }
+}
--- Lab Number6/Program.cs
+++ Lab Number6/Program.cs
@@ -11,21 +11,40 @@ namespace Lab_Number6
         static Random rnd = new Random();
         static void Main(string[] args)
         {
+            string response = "";
+            Console.WriteLine("Welcome to the Academic Games of Grand Circus!");
+            //ask user for dice sides
+            Console.WriteLine("Roll the dice? (yes or no?)");
+            response = Console.ReadLine().ToLower();
+
+
             while (true)
             {
-                Console.WriteLine("Welcome to the Academic Games of Grand Circus!");
-                //ask user for dice sides
-                Console.WriteLine("How many sides are there?");
+
+                if (response == "no")
+                {
+                    return;
+                }
+                Console.WriteLine("How many sides should each die have?");
+
+                
+
                 //asking the string to hold sides
                 string sides = Console.ReadLine();
                 //converting sides
                 int numberSides = int.Parse(sides);
-                Console.WriteLine("Do you want to roll the dice or exit?");
-                string response = Console.ReadLine();
 
+                int RollNum1 = Rolldice(numberSides);
+                int RollNum2 = Rolldice(numberSides);
+
+                DiceRollerApp.CheckRoll(RollNum1, RollNum2);
+                Console.WriteLine($"First roll is {RollNum1}, Second roll is {RollNum2}");
                 //prompt user for dice roll
                 while (true)
                 {
+
+                    Console.WriteLine("Do you want to roll the dice again or exit?");
+                    response = Console.ReadLine();
                     if (response == "exit")
                     {
                         Console.WriteLine("Thank you for participating! Goodbye");
@@ -38,57 +57,48 @@ namespace Lab_Number6
                     else
                     {
                         Console.WriteLine("Sorry, that method isn't valid. Please enter 'y' or exit.");
-                        response = Console.ReadLine();
-
-                    }
-                }
-                int rolledNum = Rolldice(numberSides);
-                int rolledNum2 = Rolldice(numberSides);
-
-
-
-                Console.WriteLine($"1st number {rolledNum} 2nd number {rolledNum2}");
-                //display what they have rolled
-
-                //ask if user wants to continue
-                Console.WriteLine("Play again?");
-                response = Console.ReadLine();
-               
-
-                while (true)
-                {
-                    if (response == "exit")
-                    {
-                        Console.WriteLine("Thank you for participating! Goodbye");
-                        return;
-                    }
-                    else if (response == "y")
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Sorry, that method isn't valid. Please enter 'y' or exit.");
-                        response = Console.ReadLine();
 
                     }
                 }
 
             }
+            //ask if user wants to continue
+            Console.WriteLine("Play again?");
+            response = Console.ReadLine();
+
+
+            while (true)
+            {
+                if (response == "exit")
+                {
+                    Console.WriteLine("Thank you for participating! Goodbye");
+                    return;
+                }
+                else if (response == "y")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, that method isn't valid. Please enter 'y' or exit.");
+                    response = Console.ReadLine();
+
+                }
+            }
         }
 
         static int Rolldice(int numberSides)
         {
-            int num = rnd.Next(1,numberSides);
+            int num = rnd.Next(1, numberSides + 1);
 
             return num;
-            
+
 
         }
 
 
     }
-
-    
-
 }
+
+
+
